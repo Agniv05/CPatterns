799e65c4b334fce2d556e57f903e684b149f2294
Agniv <122878493+Agniv05@users.noreply.github.com>
2024-10-15 16:52:08 +0530

    Update patt3.c

--- patt3.c
+++ patt3.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+int main(){
+  int n,i,j,k,t,b;
+  printf ("Enter no. of rows :");
+  scanf ("%d",&n);
+  int arr[n];
+  arr[0]=0;
+  arr[1]=1;
+  i=0;
+  j=1;
+  for (k=2;k<=n;k++){
+    arr[k]=i+j;
+    i=j;
+    j=arr[k];
+  }
+  for (t=1;t<=n;t++){
+    for (b=0;b<t;b++){
+      printf ("%d",arr[b]);
+    }
+    printf ("\n");
+  }
+}
 
+
+
+//output  for input 5
+//  Enter no. of rows: 5
+  //0
+  //01
+  //011
+  //0112
+  //01123
