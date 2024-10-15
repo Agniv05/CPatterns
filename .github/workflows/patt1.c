23b743d30cfd5a6773c0eb798310320fafc6d816
Agniv <122878493+Agniv05@users.noreply.github.com>
2024-10-15 16:28:08 +0530

    Update patt1.c

--- .github/workflows/patt1.c
+++ .github/workflows/patt1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main(){
+  int n;
+  int i,j;
+  printf ("Enter no. of rows:");
+  scanf ("%d",&n);
+  for (i=1;i<=n;i++){
+    for (j=1; j<=i;j++){
+      printf ("%s","*");
+    }
+    printf ("\n");
+  }
+}
 
+
+//output: input 5
+//*
+//**
+//***
+//****
+//*****
